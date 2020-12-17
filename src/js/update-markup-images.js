954dfe541d36324ff971acea79968d599d45f9d5
Yegor Gangalo <gangaloyegor@gmail.com>
2020-12-17 20:25:01 +0200

    fixed addEventListener to open lightBox

--- src/js/update-markup-images.js
+++ src/js/update-markup-images.js
@@ -1,50 +1,39 @@
-import imageHandleBar from '../template/image-template.hbs'
-/* ------------------------------------------------------ */
 import * as basicLightbox from 'basiclightbox';
+import imageHandleBar from '../template/image-template.hbs'
 /* ------------------------------------------------------ */
 
 export default {
     refs: {
         ul: document.querySelector('.gallery'),
     },
+    arrayPhoto: [],
+
+    clearMarkupImages() {
+        this.refs.ul.textContent = '';
+    },
 
     updateMarkupImages(dataObj) {
         const markUp = imageHandleBar(dataObj);
         this.refs.ul.insertAdjacentHTML('beforeend', markUp);
-        /* ------------------------------------------------------ */
-        const ArrayPhoto = document.querySelectorAll('.photo-card img');
-        ArrayPhoto.forEach(photo => photo.addEventListener('click', openBigImage));
-        function openBigImage() {
-            console.log('time');
-            const srcBigImg = this.dataset.fullSize;
-            const lightBox = basicLightbox.create(`
-        		<img width="1400" height="900" src="${srcBigImg}">
-            `);
-            lightBox.show();
-        };
-        /* ------------------------------------------------------ */
+        this.onImgClickAddEventLightboxOpen();
     },
 
-    clearMarkupImages() {
-        this.refs.ul.textContent = '';
-    }
+    onImgClickAddEventLightboxOpen() {
+        const arrayPhotoOldLength = this.arrayPhoto.length;
+        const collectionPhotoNew = document.querySelectorAll('.photo-card img');
+        const arrayPhotoNew = [...collectionPhotoNew];
+        arrayPhotoNew.splice(0, arrayPhotoOldLength);
+        this.arrayPhoto = [...arrayPhotoNew];
+        // this.arrayPhoto.forEach(photo => photo.removeEventListener('click', openBigImage)); //чому не працює?
+        this.arrayPhoto.forEach(photo => photo.addEventListener('click', this.openLightboxBigImage));
+        // ArrayPhoto.forEach(photo => photo.onclick = openBigImage);// можна і так, але тоді більше нічого не повісиш на елемент, зате не накладаютсья події
+    },
+
+    openLightboxBigImage({ target }) {
+        const srcBigImg = target.dataset.fullSize;
+        const lightBox = basicLightbox.create(`
+    		<img width="1400" height="900" src="${srcBigImg}">
+        `);
+        lightBox.show();
+    },
 };
-
-
-/* ------------------------------------------------------ */
-// const photoBlock = document.querySelector('.photo-card image');
-// photoBlock.addEventListener('click', openBigImage);
-
-// function openBigImage() {
-//     basicLightbox.create(`
-// 		<img width="1400" height="900" src="${this.data-full-size}">
-// 	`).show();
-// };
-/* ------------------------------------------------------ */
-// document.querySelector('button.image').onclick = () => {
-
-//   basicLightbox.create(`
-// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
-// 	`).show()
-
-// };
