aa9b950a03f20e9c6457988d96d1b03b4bf470a9
Yegor Gangalo <gangaloyegor@gmail.com>
2020-12-23 13:35:32 +0200

    add staticurl

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -4,6 +4,7 @@ export default {
 _page: 1,
 per_page: 15,
 searchQuery: '',
+staticURL: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
 
 fetchFirst(queryValue) {
   this.resetPage();
@@ -17,7 +18,7 @@ fetchMore() {
 },
 
 fetchQuery(query) {
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=${this.page}&per_page=${this.perPage}&key=${keyAPI}`;
+  const url = `${this.staticURL}&q=${query}&page=${this.page}&per_page=${this.perPage}&key=${keyAPI}`;
   return fetch(url)
     .then(res => {
       return res.json();
